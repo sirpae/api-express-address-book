be7aadb7ece7e53a05bc24f9e647ff3c59ddbee8
p <101421945+sirpae@users.noreply.github.com>
2022-08-03 22:00:20 +0100

    Uncompleted.

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
 const contacts = require("./data/contacts")
-
-
 const app = express()
-
 app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
