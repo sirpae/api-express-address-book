bcebf5a7ee4d66b9d4e042cc58f01bf9fb2796eb
p <101421945+sirpae@users.noreply.github.com>
2022-08-21 21:16:54 +0100

    Tests

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const cors = require("cors")
 const contacts = require("./data/contacts")
 const meetings = require("./data/meetings")
 
-
 const app = express()
 app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
-
 
 
 // Default / route
@@ -18,34 +16,39 @@ app.get("/", (req, res) => {
     res.json({ msg: "Hello!" });
   });
 
-// Get '/contacts'
+// Get & Post Contacts
 app.get("/contacts", (req, res) => {
     res.json({
         "contacts": contacts
     })
 })
 
-// Get & Post Contacts via ID
+app.post('/contacts', (req, res) => {
+    const createdContact = {
+        ...req.body,
+        id: contacts.length + 1
+    }
+
+    contacts.push(createdContact)
+
+    res.status(201).json({
+        contact: {
+            ...createdContact,
+            meetings: []
+        }
+    })
+})
+
+// Get Contacts via ID
 app.get("/contacts/:id", (req, res) => {
     const contact = contacts.find((item) => item.id === Number(req.params.id));
     res.json({ contact: contact });
   });
 
-  app.post("/contacts/:id", (req, res) => {
+app.post("/contacts/:id", (req, res) => {
     const contact = contacts.find((item) => item.id === Number(req.params.id));
     res.json({ contact: contact });
   });
-
-// -----------------------------------------------------------------//
-app.post('/contacts', (req, res) => {
-    // Creates the Contact
-    res.json({ contact: {} })
-})
-app.get('/contacts/:id', (req, res) => {
-    // Get contact with the id provided from the contacts array
-    res.json({ message: "Great!" })
-})
-// -----------------------------------------------------------------//
 
 
 // Get & Post Meetings
@@ -60,15 +63,8 @@ app.post('/meetings', (req, res) => {
 })
 
 
-
-
-
-
-
-
 //Start up our server
 const port = 3030
 app.listen(port, () => {
  console.log(`Server is running on http://localhost:${port}/`)
 })
-
